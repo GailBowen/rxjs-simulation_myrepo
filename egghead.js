4ee39f5c0c86f51fb4efd60611ca57b1b07eeb6a
GailFoad <gail.foad@thenavpeople.co.uk>
2024-02-15 17:25:56 +0000

    an observable that emits values every 500 millisecond

--- egghead.js
+++ egghead.js
@@ -1,12 +1,16 @@
 
 // ES6 code
-import { from } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { from, interval } from 'rxjs';
+import { map, filter, take } from 'rxjs/operators';
 
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const observable = from(data);
+console.clear();
 
-observable
+// const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const observable = from(data);
+
+var source = interval(500).pipe(take(10));
+
+source
   .pipe(
     filter(item => item % 2 === 1),
     map(item => item + '!'),
