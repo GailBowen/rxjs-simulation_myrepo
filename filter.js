68bb4da4ee19bfa636c9349c1e3e78fdfe0917f7
GailFoad <gail.foad@thenavpeople.co.uk>
2024-02-16 12:40:01 +0000

    foreach vs filter

--- filter.js
+++ filter.js
@@ -1,6 +1,6 @@
 
 var stockInfo = [
-    {price: 550, symbol: "AAPL", volume: 900},   
+    {price: 550, symbol: "WETH", volume: 900},   
     {price: 100, symbol: "AAPL", volume: 200},
     {price: 200, symbol: "GOOG", volume: 100},
     {price: 300, symbol: "MSFT", volume: 300},
@@ -11,15 +11,30 @@ function getStocksOver(stockInfo, min) {
     
     var results = [];
 
-    stockInfo.filter(function(item) {
+    // stockInfo.filter(function(item) {
         
+    //     if (item.price > min)
+    //     {
+    //     results.push(item.symbol);
+    //     }
+    // })
+
+    // stockInfo.filter((item) => {
+        
+    //     if (item.price > min)
+    //     {
+    //     results.push(item.symbol);
+    //     }
+    // })
+
+
+    stockInfo.forEach(function(item){
         if (item.price > min)
         {
-        results.push(item.symbol);
+          results.push(item.symbol);
         }
+        
     })
-
-    //stockInfo.forEach
     
     return results;
 }
