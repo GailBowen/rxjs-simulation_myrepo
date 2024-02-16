847507cd6d83968703ed50dafa434a92422b27f7
GailFoad <gail.foad@thenavpeople.co.uk>
2024-02-16 18:17:38 +0000

    not working!

---
+++ concatAll.js
@@ -0,0 +1,37 @@
+var exchanges = [
+    [
+        {price: 200, symbol: "GOOG", volume: 100},
+        {price: 300, symbol: "MSFT", volume: 300},
+        {price: 400, symbol: "AMZN", volume: 400},
+    ],
+    [
+        {price: 200, symbol: "WETH", volume: 100},
+        {price: 300, symbol: "TAIL", volume: 300},
+        {price: 400, symbol: "CAT", volume: 400},
+    ]
+];
+
+// exchanges.forEach(function (exchange) {
+//     //console.log(JSON.stringify(exchange));
+
+//     exchange.forEach(function(innerExchange){
+//         console.log(JSON.stringify(innerExchange));
+//     })
+
+// });
+
+var infos = exchanges.concatAll();
+
+Array.prototype.concatAll = function() {
+    var results = [];
+    
+    this.forEach(function(subArray) {
+      subArray.forEach(function(item) {
+        results.push(item);    
+      });
+    });  
+  
+    return results;
+  };
+  
+
--- filter.js
+++ filter.js
@@ -1,7 +1,6 @@
 
 var stockInfo = [
-    {price: 550, symbol: "WETH", volume: 900},   
-    {price: 100, symbol: "AAPL", volume: 200},
+                                     
     {price: 200, symbol: "GOOG", volume: 100},
     {price: 300, symbol: "MSFT", volume: 300},
     {price: 400, symbol: "AMZN", volume: 400},
